fd5b3c62b959e5c64de58e3df785e962888c1cae
kaddemaxiv <adamjglarsson@gmail.com>
2019-06-17 13:30:30 +0200

    Exporter can now export data from multible IcePAP racks. Also fixed temp_gauges to a class variable

--- promExporter.py
+++ promExporter.py
@@ -10,41 +10,42 @@ class promExporter:
 	# read data from IcePAPs at adress 'ip'
 	def __init__(self, ip):
 		self.ip = str(ip)
+		self.temp_gauges = []
 		self.use_ip = self.ip.replace('-','_')
-		print 'ip: ' + self.ip
-		print 'use_ip: ' + self.use_ip
 		self.ice = IceParser(self.ip)
+
  
 
 
 	# Processes the temperature of the IcePAPs on the rack
 	# with adress 'ip'. 
 	def setup_temperature_gauge(self):
- 		temp_gauge = []
-
 		for card in self.ice.getCardsAlive():
 			card = str(card)
-			print self.use_ip +  '_icepap_' + card
-			temp_gauge.append(Gauge(self.use_ip +  '_icepap_' + card + '_temperature' , 'Temperature of the IcePAP'))
+			self.temp_gauges.append(Gauge(self.use_ip +  '_icepap_' + card + '_temperature' , 'Temperature of the IcePAP'))
 	
-		return temp_gauge
+		
 
-	def request_icepap_temperature(self, temp_guages):
+	def request_icepap_temperature(self):
 		temps = []
 		temps = self.ice.getCardTemps()
-		for i in range(len(temp_guages)):
-				temp_guages[i].set(temps[i])
+		for i in range(len(self.temp_gauges)):
+				self.temp_gauges[i].set(temps[i])
 
 	
 
 
 def main():
-	ex = promExporter('w-kitslab-icepap-11')
-	temp_gauges = ex.setup_temperature_gauge()
+	ips = ['w-kitslab-icepap-11', 'w-kitslab-icepapcmstst-icepap-12']
+	exporters = []
+	for i in range(len(ips)):
+		exporters.append(promExporter(ips[i]))
+		exporters[i].setup_temperature_gauge()
 	start_http_server(6122)
 	while True:
 		try:
-			ex.request_icepap_temperature(temp_gauges)
+			for exporter in exporters:
+				exporter.request_icepap_temperature()
 		except KeyboardInterrupt:
 			print "\nClosing"
 			sys.exit(0)
